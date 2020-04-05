6e8d8c3ff867ee8019abfefc7f460e290ddc5097
Girma Nigusse <xgirma@gmail.com>
2020-04-05 17:47:23 -0400

    update

--- spec/sandbox.spec.js
+++ spec/sandbox.spec.js
@@ -1,5 +1,6 @@
 import { chromium } from "playwright";
-import { expect } from "chai";
+// eslint-disable-next-line no-unused-vars
+const should = require("chai").should();
 
 let page;
 let browser;
@@ -26,9 +27,11 @@ describe("Sandbox", () => {
 
   it("should be on sandbox", async () => {
     await page.waitFor("h1");
+
+    const pageTitle = await page.title();
     const title = await page.$eval("h1", (el) => el.textContent);
 
-    expect(await page.title()).to.equal("Sandbox");
-    expect(title).to.equal("Sandbox");
+    pageTitle.should.eql("Sandbox");
+    title.should.eql("Sandbox");
   });
 });
